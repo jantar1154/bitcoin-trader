160104f2f3ae04e852ee7fc2a87d05bcf9e5de77
Jantar <davidbednarek@email.cz>
2021-09-21 12:32:55 +0200

    upravena dokumentace, udělán základ

--- web/script.js
+++ web/script.js
@@ -1,7 +1,20 @@
 var bitcoinPrice = 950000;
+var bitcoinPriceLast = 0;
+var bitcoinChange = 0;
 
 setInterval(loop, 10000);
 
 function loop(){
-	Document.getElementById("btcPrice").innerHTML = bitcoinPrice + " Kč";
+	document.getElementById("btcPrice").innerHTML = bitcoinPrice + ",- Kč";
+	document.getElementById("btcChange").innerHTML = bitcoinChange + ",- Kč";
+	
+	bitcoinChange = Math.round(bitcoinPriceLast - bitcoinPrice, 1);
+	bitcoinPriceLast = bitcoinPrice;
+	bitcoinPrice += change(1000);
+}
+
+function change(mul){
+	let r = Math.round(Math.random() * mul);
+	r *= 1 - Math.random() * 2;
+	return Math.round(r);
 }
